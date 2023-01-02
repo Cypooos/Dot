1e7df981f4edadb4b55b87e8bb556b77d2da2c00
Cypooos <60020572+Cypooos@users.noreply.github.com>
2023-01-02 15:50:34 +0100

    space mistake

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tmux has-session -t dot_bot 2>/dev/null
-if [$? != 0]; then
+if [ $? != 0]; then
     git pull
     tmux new-session -d -s dot_bot
     tmux send-keys -t dot_bot "python3 main.py" Enter
