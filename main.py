3b0e8662a8c921be0e3c2ecfa70bea8352f60e65
Cypooos <60020572+Cypooos@users.noreply.github.com>
2023-01-02 16:31:02 +0100

    Updated emogies to be fetched for each guild

--- main.py
+++ main.py
@@ -7,49 +7,45 @@ tree = app_commands.CommandTree(client)
 
 with open("./key.env","r") as f:
     TOKEN = f.read()
-with open("./guild.env","r") as f:
-    GUILD_ID = int(f.read())
-
-GUILD = discord.Object(id=GUILD_ID)
+    
 print("TOKEN:",TOKEN[:5].upper())
-print("GUILD_ID:",str(GUILD_ID)[:5])
 
-GUILD = None
-
+GUILDS_PRIORITARY = [ discord.Object(id=x) for x in [1059253745906622545,1057772277967638538,959879578019577887] ]
 ZU_EMOGIES = { }
 
 
-@tree.command(name = "ping", description = "It says pong !",guild=discord.Object(id=GUILD_ID)) 
+@tree.command(name = "ping", description = "It says pong !", guilds=GUILDS_PRIORITARY) 
 async def ping(interaction):
     await interaction.response.send_message("pong!")
     
-@tree.command(name = "say", description = "Make Dot say something",guild=discord.Object(id=GUILD_ID)) 
+@tree.command(name = "say", description = "Make Dot say something", guilds=GUILDS_PRIORITARY) 
 async def say(interaction,text:str):
     await interaction.response.send_message(text)
 
-@tree.command(name = "zuify", description = "Translate a text to zu!",guild=discord.Object(id=GUILD_ID)) 
+@tree.command(name = "zuify", description = "Translate a text to zu!", guilds=GUILDS_PRIORITARY) 
 async def zuify(interaction, text:str):
     t = [c.upper() for c in text.strip()]
-    out = map(lambda x:str(ZU_EMOGIES.get(x,":question:")),t)
+    out = map(lambda x:str(ZU_EMOGIES[interaction.guild_id].get(x,":question:")),t)
     await interaction.response.send_message("".join(out))
 
 
 @client.event
 async def on_ready():
     global GUILD
-    GUILD = client.get_guild(GUILD_ID)
-    if GUILD == None:
-        print("GUILD NOT JOINED I AM DED")
-        quit()
-    await tree.sync(guild=discord.Object(id=GUILD_ID))
-    
 
-    for c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-        name_ = "zu"+c
-        if c == "Q" or c == "K": name_ = "zuKQ"
-        if c == "U" or c == "V": name_ = "zuUV"
-        ZU_EMOGIES[c] = discord.utils.get(GUILD.emojis, name=name_)
-    ZU_EMOGIES[" "] = discord.utils.get(GUILD.emojis, name="zu")
+    for guild in client.guilds:
+        print("Adding zu alphabet for",guild.name)
+        ZU_EMOGIES[guild.id] = {}
+        for c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+            name_ = "zu"+c
+            if c == "Q" or c == "K": name_ = "zuKQ"
+            if c == "U" or c == "V": name_ = "zuUV"
+            ZU_EMOGIES[guild.id][c] = discord.utils.get(guild.emojis, name=name_)
+        ZU_EMOGIES[guild.id][" "] = discord.utils.get(guild.emojis, name="zu")
+
+        print("Syncing tree for",guild.name)
+        await tree.sync(guild=guild)
+    
 
     print("Ready!")
 
