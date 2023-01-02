897765e8362e7efd7d0e59a29ad12c0065386d22
Cypooos <60020572+Cypooos@users.noreply.github.com>
2023-01-02 15:52:51 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 tmux has-session -t dot_bot 2>/dev/null
-if [ $? != 0]; then
+if [ $? != 0 ]; then
     git reset --hard HEAD
     git pull
+    chmod 755 ./start.sh
     tmux new-session -d -s dot_bot
     tmux send-keys -t dot_bot "python3 main.py" Enter
 fi
